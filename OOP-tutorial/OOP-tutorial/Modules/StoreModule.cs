ac6fcf4f5cea85b30edcf121b6aaf06c6060a7dd
Carmela Manalo <xtremex471@gmail.com>
2021-04-02 01:21:03 +0800

    Added a new folder for the modules. Added a StoreModule and installed mvvmlightlibs.

---
+++ OOP-tutorial/OOP-tutorial/Modules/StoreModule.cs
@@ -0,0 +1,13 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace OOP_tutorial.Modules
+{
+    public class StoreModule : ObservableObject
+    {
+        public ObservableCollection<Thing> Things { get; } = new ObservableCollection<Thing>();
+    }
+}
