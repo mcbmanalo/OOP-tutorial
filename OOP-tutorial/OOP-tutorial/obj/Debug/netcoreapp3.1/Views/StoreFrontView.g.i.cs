b5dc9fed9762dbdd0dedb9d345f319a1eb1e04a2
Carmela Manalo <xtremex471@gmail.com>
2021-04-02 13:26:50 +0800

    Added an AddThingView.xaml for the UI for adding Things into the store. Binded the different properties from the store module to the AddThingView.xaml

--- OOP-tutorial/OOP-tutorial/obj/Debug/netcoreapp3.1/Views/StoreFrontView.g.i.cs
+++ OOP-tutorial/OOP-tutorial/obj/Debug/netcoreapp3.1/Views/StoreFrontView.g.i.cs
@@ -53,7 +53,7 @@ namespace OOP_tutorial.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/OOP-tutorial;component/views/storefrontview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/OOP-tutorial;V1.0.0.0;component/views/storefrontview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\StoreFrontView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
