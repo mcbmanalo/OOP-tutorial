b1fcbe9baa8bc1764a2ff6094222152315508914
Carmela Manalo <xtremex471@gmail.com>
2021-04-02 13:39:00 +0800

    Added a ThingTypeConverter.cs for the visibility of the StackPanels in the AddThingView.

---
+++ OOP-tutorial/OOP-tutorial/ThingTypeConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace OOP_tutorial
+{
+    public class ThingTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string selectedParameter = parameter as string;
+            if (value != null)
+            {
+                if (value.ToString() == selectedParameter )
+                {
+                    return Visibility.Visible;
+                }
+                else
+                {
+                    return Visibility.Collapsed;
+                }
+            }
+
+            else
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
